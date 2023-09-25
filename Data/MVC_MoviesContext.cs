4f6541b383d6a831db6b43f5b9c7be9459e13f20
W0834082 <W0834082@ADL-A0218Z-18>
2023-09-25 15:34:58 -0400

    Add project files.

---
+++ Data/MVC_MoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MVC_Movies.Data
+{
+    public class MVC_MoviesContext : DbContext
+    {
+        public MVC_MoviesContext (DbContextOptions<MVC_MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
